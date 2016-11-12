49d5eef0e2597de978bb40f3738ccb2b18210b41
cristi <joracristi@gmail.com>
2016-11-12 11:17:06 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    {{payments | json}}
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data(){
+  return {
+    payments:["test"]
+  }
+
+  },
+  mounted(){
+  var socket = io('http://localhost:3000');
+  var self=this;
+  socket.on('payment', function (data) {
+    self.payments.push(data);
+    console.log(data);
+  });
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
